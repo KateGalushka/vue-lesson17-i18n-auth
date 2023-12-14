396f4541140cc99dd44d0c6432d456b7c0be38fe
Kate Galushka <kgalushka@gmail.com>
2023-12-14 19:22:07 +0200

    task done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    root: true,
-    env: {
+   root: true,
+   env: {
         node: true,
     },
 	extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-    parserOptions: {
+   parserOptions: {
         parser: '@babel/eslint-parser',
     },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+   rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     },
 }
